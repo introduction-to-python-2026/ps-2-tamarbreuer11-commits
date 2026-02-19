f67a056b948ac9a78d8312e562352990051c4346
tamarbreuer11-commits <tamarbreuer11@gmail.com>
2026-02-19 18:01:35 +0200

    Add functions to find max, mean, and std deviation
    Implemented functions to find maximum number, mean, and standard deviation.

--- math_utils.py
+++ math_utils.py
@@ -1,10 +1,14 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
-
+  if (num1>=num2 and num1>=num3):
+    return num1
+  if (num2>=num3):
+    return num2
+  return num3
 def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
-
+    meanv = (num1+num2+num3)/3
+    return meanv
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    pass  # Replace 'pass' with code
+    std = (((num1-mean)**2+(num2-mean)**2+(num3-mean)**2)/3)**0.5
+    return mean,std
 
